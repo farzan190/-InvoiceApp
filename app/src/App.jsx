ff3f8bee4b88322e570b6e20b99cecd649d79aa3
farzan190 <farzanxavier@gmail.com>
2024-12-01 16:31:15 +0530

    completed tabs styling

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,66 @@
 import Tabs from "./components/Tabs";
 import invoiceData from './utils/data.json';
+import { useState } from "react";
 function App() {
-  return <div>
-    <div>{ invoiceData.map((invoice)=> <Tabs id={invoice.id} createdAt={invoice.createdAt} clientName={invoice.clientName} status={invoice.status}/>)}</div>
+  const[invoices,setInvoices]=useState(invoiceData);
+
+  const handleFilter=(e)=>{
+    console.log(e.target.id);
+    const Draft=invoices.filter((item)=>item.status=="pending");
+    setInvoices(Draft);
+  }
+
+  const addInvoice=()=>{
+    setInvoices([...invoices,{
+      "id": "RT3080",
+      "createdAt": "2021-8-18",
+      "paymentDue": "2021-8-19",
+      "description": "Re-branding",
+      "paymentTerms": 1,
+      "clientName": "PHAJJU",
+      "clientEmail": "jensenh@mail.com",
+      "status": "paid",
+      "senderAddress": {
+        "street": "19 Union Terrace",
+        "city": "London",
+        "postCode": "E1 3EZ",
+        "country": "United Kingdom"
+      },
+      "clientAddress": {
+        "street": "106 Kendell Street",
+        "city": "Sharrington",
+        "postCode": "NR24 5WQ",
+        "country": "United Kingdom"
+      },
+      "items": [
+        {
+          "name": "Brand Guidelines",
+          "quantity": 1,
+          "price": 1800.90,
+          "total": 1800.90
+        }
+      ],
+      "total": 1800.90
+    }])
+  }
+
+
+  return <div className="homepage">
+    <nav>
+      <div className="right-nav">
+      <div className="Invoices-heading">Invoices</div>
+      <div>There are total 7 Invoices</div>
+      </div>
+      <div className="left-nav">
+      <div><select onChange={(e)=>handleFilter(e)}>
+      <option value="Draft">Draft</option>
+      <option value="Pending">Pending</option>
+      <option value="Paid">Paid</option>
+    </select></div>
+    <button onClick={addInvoice} className="AddInvoice">AddInvoice</button>
+    </div>
+    </nav>
+    <div className="Invoices">{ invoices.map((invoice)=> <Tabs id={invoice.id} createdAt={invoice.createdAt} clientName={invoice.clientName} itemTotal={invoice.total} status={invoice.status}/>)}</div>
      </div>
 }
 
--- app/src/components/Tabs.jsx
+++ app/src/components/Tabs.jsx
@@ -1,9 +1,18 @@
-const Tabs=({id,createdAt,clientName,status})=>{
-    return <div className="slip"> 
-         <div>{id} </div>
-         <div>{createdAt}</div>
-         <div>{clientName}</div>
-         <div>{status}</div>
+import Arrowimg from '../utils/icon-arrow-right.svg' ;
+
+
+const Tabs=({id,createdAt,clientName,itemTotal,status})=>{
+    return <div className="Tab"> 
+         <div className="left-details">
+         <div className="bill-Id">{id} </div>
+         <div className="createdAt">{createdAt}</div>
+         <div className="clientName">{clientName}</div>
+         </div>
+         <div className="right-details">
+         <div className="itemTotal">£{itemTotal}</div>
+         <button className="status-button"><div className="status-indicator"></div>{status}</button>
+         <img src={Arrowimg} alt='arrow-image'/>
+         </div>
     </div>
 }
 export default Tabs;
